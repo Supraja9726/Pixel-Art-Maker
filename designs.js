23eed6111d9070cb14d86ee487933f232c5f08e1
Supraja <supraja.b.2015.cse@rajalakshmi.edu.in>
2018-05-12 21:10:08 +0530

    final

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
 // Select color input
+var colorInput = $("#colorPicker").val();
 // Select size input
+var button = $("#button, input[type='submit']").val();
+var canvas = $("#pixelCanvas").val();
 
 // When size is submitted by the user, call makeGrid()
-
+// Builds a Grid
 function makeGrid() {
-
-// Your code goes here!
-
+	 var row = $("#inputHeight").val();
+	 var col = $("#inputWeight").val();
+     var td = "<td></td>".repeat(col);
+     canvas.empty();
+     for (row; row > 0; row--) {
+     	canvas.append("<tr>" + td + "</tr>");
+     }
 }
+
+// Listens the ButtonClick function
+$(function(){
+	button.ButtonClick(function(g){
+		g.preventDefault();
+		makeGrid();
+	});
+});
+//Updates color input when new color is selected
+$("#colorPicker").change(function(event){
+	colorInput = event.target.value;
+});
+
+
+canvas.on("mousedown", "td", function(event){
+	$(this).attr("bgcolor", colorInput);
+});
